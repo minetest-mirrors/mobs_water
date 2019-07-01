d41bf23dd271944f6da79b94d471cf672cf9ba80
TenPlus1 <kinsellaja@yahoo.com>
2019-07-01 17:34:58 +0100

    5 fish per area instead of 1, need more fishy

--- mobs_fish/init.lua
+++ mobs_fish/init.lua
@@ -67,7 +67,7 @@ local SPRITE_VERSION = false	-- set to true to use upright sprites instead of me
 		end
 	})
 	--name, nodes, neighbours, minlight, maxlight, interval, chance, active_object_count, min_height, max_height
-	mobs:spawn_specific("mobs_fish:clownfish", l_spawn_in, l_spawn_near, 5, 20, 30, l_spawn_chance, 1, -31000, l_water_level)
+	mobs:spawn_specific("mobs_fish:clownfish", l_spawn_in, l_spawn_near, 5, 20, 30, l_spawn_chance, 5, -31000, l_water_level)
 	mobs:register_egg("mobs_fish:clownfish", "Clownfish", "animal_clownfish_clownfish_item.png", 0)
 
 -- Tropical fish
@@ -98,7 +98,7 @@ local SPRITE_VERSION = false	-- set to true to use upright sprites instead of me
 		end
 	})
 	--name, nodes, neighbours, minlight, maxlight, interval, chance, active_object_count, min_height, max_height
-	mobs:spawn_specific("mobs_fish:tropical", l_spawn_in, l_spawn_near, 5, 20, 30, l_spawn_chance, 1, -31000, l_water_level)
+	mobs:spawn_specific("mobs_fish:tropical", l_spawn_in, l_spawn_near, 5, 20, 30, l_spawn_chance, 5, -31000, l_water_level)
 	mobs:register_egg("mobs_fish:tropical", "Tropical fish", "animal_fish_blue_white_fish_blue_white_item.png", 0)
 
 end
